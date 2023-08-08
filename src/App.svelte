2a8d66e7b1190e5d35e112e5e04e7973b25e35b7
Daniel Graziani <59689758+danielgraziani91@users.noreply.github.com>
2023-08-08 12:20:11 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
-<div class="font-montserrat p-8">
+<script lang="ts">
+  import NavigationLink from "./components/NavigationLink.svelte";
+</script>
+
+
+<div class="font-montserrat p-8 bg-blue-400">
   <header class="flex justify-around">
     <h1 class="font-bold text-2xl">
         BSQ
     </h1>
 
     <nav>
-      <a class="text-white">Home</a>
-      <a class="text-white">Home</a>
-      <a class="text-white">Home</a>
+      <NavigationLink href="home" text="Home" />
+      <NavigationLink href="about_us" text="Home" />
     </nav>
   </header>
 
   <main>
-    <p class="text-2xl">Tua mamma si droga</p>
+    <p class="text-2xl">asd</p>
   </main>
 </div>
+
+<div id="test"></div>
--- src/components/NavigationLink.svelte
+++ src/components/NavigationLink.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
   export let href: string, text: string;
+
+  let underline = false;
+
+  function handleClick() {
+    const words = window.location.href.split('#')
+    console.log(window.location.href, words[1]);
+    return words[1] === href
+  }
 </script>
 
-<a {href} class="text-white underline-offset-8" class:underline={}>
+<a href={`#${href}`} on:click={handleClick} class="text-white underline-offset-8" class:underline={handleClick}>
   {text}
 </a>
