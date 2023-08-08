ffaada6d6468a732b4c094affcec1417d00287ab
Federico Di Francesco <difrancescofederico@gmail.com>
2023-08-08 13:07:07 +0200

    ..

--- src/components/NavigationLink.svelte
+++ src/components/NavigationLink.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   export let href: string, text: string;
 
-  let underline = false;
+  let underline = false
 
   function handleClick() {
     const words = window.location.href.split('#')
-    console.log(words[1] == href)
-    return words[1] === href
+    underline = words[1] == href
   }
 </script>
 
-<a href={`#${href}`} on:click={handleClick} class="text-white underline-offset-8" class:underline={handleClick}>
+<a href={`#${href}`} on:click={handleClick} class="text-white underline-offset-8" class:underline={underline}>
   {text}
 </a>
