9aca54089a1c669ea9da276e4e162963a11aa335
Daniel Graziani <danielgraziani91@gmail.com>
2023-09-05 10:52:04 +0200

    add navigation link and margin

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,7 +10,8 @@ class:bg-gray-700={$scrolled}
 >
   <h1 class="font-bold text-2xl text-white">BSQ</h1>
 
-  <nav class="flex lg:gap-x-12 text-sm text-md gap-x-4 text-center uppercase">
+  <nav class="flex lg:gap-x-12 text-sm text-md gap-x-8 text-center uppercase mt-5 lg:mt-0">
+    <NavigationLink href="/" text="Home" />
     <NavigationLink href="#about_us" text="About us" />
     <NavigationLink href="#our_services" text="Our services" />
     <NavigationLink href="#learning" text="Learning" />
